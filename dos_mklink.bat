367f11adf1fdfa22510ddb98c8eff20a845a2c95
Takubo Morio <takubo.morio@gmail.com>
2019-06-10 01:04:30 +0900

    mklinkで、USERNAMEを使用するようにした
    startupのSimlinkを$VIMに作るようにした

--- dos_mklink.bat
+++ dos_mklink.bat
@@ -1,8 +1,12 @@
 C:
-cd \Users\Morio
+cd C:\cygwin\home\%USERNAME%
 mklink .vimrc vimfiles\.vimrc
 mklink .gvimrc vimfiles\.gvimrc
 
 C:
-cd C:\Users\Morio\bin\vim81-kaoriya-win64\
-mklink vimrc_local.vim C:\cygwin\home\Morio\vimfiles\vimrc_local.vim
+cd C:\Users\%USERNAME%\bin\vim81-kaoriya-win64\
+mklink vimrc_local.vim C:\cygwin\home\%USERNAME%\vimfiles\vimrc_local.vim
+mklink startuptime.bat C:\cygwin\home\%USERNAME%\vimfiles\startuptime.bat
+
+
+pause
