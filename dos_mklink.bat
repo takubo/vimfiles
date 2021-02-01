d6c18901162e0613054f5bcc735b45935a259cd4
Takubo Morio <takubo.morio@gmail.com>
2021-02-01 18:15:36 +0900

    dos_mklink.bat: Win10のセキュリティ回避
    Win10は、管理者でないとシンボリックリンクを作れないことの回避。
    管理者として、シンボリックリンクを作ったとしても、一般ユーザーでは、そのリンクを辿れないようだ。
    
    結果、ハードリンクとすることで、すべて解決できた(ように見える)。
    所詮Win環境なので動きさえすればよい。まともに相手するのは、時間の無駄。

--- dos_mklink.bat
+++ dos_mklink.bat
@@ -2,6 +2,7 @@ rem リンクを作る場所へ移動する
 C:
 rem cd C:\cygwin*\home\%USERNAME%
 cd C:\cygwin64\home\%USERNAME%
+rem こっちはハードリンクでなくてよいのか、要確認。
 mklink .vimrc vimfiles\.vimrc
 mklink .gvimrc vimfiles\.gvimrc
 
@@ -9,8 +10,14 @@ mklink .gvimrc vimfiles\.gvimrc
 rem リンクを作る場所へ移動する
 C:
 cd C:\Users\%USERNAME%\bin\vim*-kaoriya-win64\
-mklink vimrc_local.vim C:\cygwin64\home\%USERNAME%\vimfiles\vimrc_local.vim
-mklink startuptime.bat C:\cygwin64\home\%USERNAME%\vimfiles\startuptime.bat
+mklink /H vimrc_local.vim C:\cygwin64\home\%USERNAME%\vimfiles\vimrc_local.vim
+mklink /H startuptime.bat C:\cygwin64\home\%USERNAME%\vimfiles\startuptime.bat
+
+
+rem DOS窓から実行したときのために戻る
+C:
+rem cd C:\cygwin*\home\%USERNAME%
+cd C:\cygwin64\home\%USERNAME%\vimfiles
 
 
 pause
